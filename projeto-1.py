67378f28c531adb442c240c744d0b09826ffcaa1
guinafelix <oguilhermeleitefelix@gmail.com>
2022-10-23 09:39:06 -0300

    update rule_3

--- projeto-1.py
+++ projeto-1.py
@@ -59,16 +59,17 @@ def rule_2(mat, atributos, q_regras):
 
 def rule_3(mat, atributos, q_regras):
     list_to_return = []
-    for i in range(q_regras):
-        for j in range(atributos):
-            if j >= atributos - 2:
-                list_to_return.append(Atom(mat[0][j] + ',' + str(i + 1) + ',' + 'gt'))
-            else:
-                list_to_return.append(Atom(mat[0][j] + ',' + str(i + 1) + ',' + 'le'))
-    return or_all(list_to_return)
-
+    for k in range(len(mat)):
+        if mat[k][q_regras + 1] == '0':
+            for i in range(q_regras):
+                for j in range(atributos):
+                    if j >= atributos - 2:
+                        list_to_return.append(Atom(mat[0][j] + ',' + str(i + 1) + ',' + 'gt'))
+                    else:
+                        list_to_return.append(Atom(mat[0][j] + ',' + str(i + 1) + ',' + 'le'))
+    return list_to_return
 
 
 test = rule_3(matriz, 3, 2)
-
-
+for a in test:
+    print(a.__str__())
